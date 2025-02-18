95c95548f2d787f6e07e00bd63abe0bd76552d20
ANTOINE METHOT <amethot@etu.uqac.ca>
2025-02-18 17:10:14 -0500

    Working token generator and attribute to client

--- MyServerSocket.java
+++ MyServerSocket.java
@@ -6,6 +6,7 @@ import java.util.Random;
 public class MyServerSocket {
 
     public ServerSocket server = null;
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     public MyServerSocket(String ipAddress) throws Exception {
         if (ipAddress != null && !ipAddress.isEmpty())
@@ -19,20 +20,37 @@ public class MyServerSocket {
         while (true) {
             Socket client = this.server.accept();
             System.out.println("\r\nNew connection from Client " + clientAddress);
-            new ClientHandler(client, clientAddress).start();
+            new ClientHandler(client, clientAddress, generateToken(20)).start();
             clientAddress += 1;
         }
     }
 
+    // ################## TOKEN GENERATOR #######################################################
+    public static String generateToken(int length) {
+        Random random = new Random(); // Simple Random
+        StringBuilder token = new StringBuilder(length);
+
+        // Generate the token
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            token.append(CHARACTERS.charAt(index));
+        }
+
+        return token.toString();
+    }
+
+    // ################## CLIENT HANDER #######################################################
     private class ClientHandler extends Thread {
         private Socket client;
         private BufferedReader in;
         private BufferedWriter out;
         private int clientNumber;
+        private String token;
 
-        public ClientHandler(Socket client, int clientNumber) throws IOException {
+        public ClientHandler(Socket client, int clientNumber, String token) throws IOException {
             this.client = client;
             this.clientNumber = clientNumber;
+            this.token = token;
             this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
             this.out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
         }
@@ -43,10 +61,18 @@ public class MyServerSocket {
                 String data;
                 while ((data = in.readLine()) != null) {
                     System.out.println("Message from client " + clientNumber + ": " + data);
-                    String response = "Hello " + data;
-                    out.write(response);
-                    out.newLine();
-                    out.flush();
+
+                    switch (data){
+                        case "REGISTER":
+                            out.write("REGISTERED: " + token);
+                            out.newLine();
+                            out.flush();
+                            break;
+                        default:
+                            out.write("INVALID COMMAND");
+                            out.newLine();
+                            out.flush();    
+                    }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -54,6 +80,7 @@ public class MyServerSocket {
         }
     }
 
+    // ########################### MAIN ###################################################################################
     public InetAddress getSocketAddress() {
         return this.server.getInetAddress();
     }
@@ -66,26 +93,25 @@ public class MyServerSocket {
         MyServerSocket app = new MyServerSocket(args[0]);
         System.out.println(
                 "Running Server: " + "Host=" + app.getSocketAddress().getHostAddress() + " Port=" + app.getPort());
-        try{
+        try {
             File Peers = new File("Peers_list.txt");
             Scanner myReader = new Scanner(Peers);
-            if(Peers.exists()){
+            if (Peers.exists()) {
                 System.out.println("Peers List: ");
-                while (myReader.hasNextLine()){
+                while (myReader.hasNextLine()) {
                     String data = myReader.nextLine();
                     String[] parts = data.split(":");
-                    String ipAddress = parts[0].trim();  // IP address
-                    String port = parts[1].trim();       // Port
+                    String ipAddress = parts[0].trim(); // IP address
+                    String port = parts[1].trim(); // Port
 
                     InetAddress address = InetAddress.getByName(ipAddress);
                     boolean reachable = address.isReachable(Integer.parseInt(port));
-                    
+
                     System.out.println(reachable ? data + " is reachable" : data + "is not reachable");
                 }
             }
             myReader.close();
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.out.println("File not Found");
             e.printStackTrace();
         }
