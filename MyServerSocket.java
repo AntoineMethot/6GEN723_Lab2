bdf6dafa1cd0d668dbbaa992123cb3a91f51f830
ANTOINE METHOT <amethot@etu.uqac.ca>
2025-02-18 16:33:58 -0500

    Ajout connexion peers

--- MyServerSocket.java
+++ MyServerSocket.java
@@ -1,14 +1,16 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class MyServerSocket {
 
     public ServerSocket server = null;
+
     public MyServerSocket(String ipAddress) throws Exception {
-        if (ipAddress != null && !ipAddress.isEmpty()) 
-          this.server = new ServerSocket(0, 1, InetAddress.getByName(ipAddress));
-        else 
-          this.server = new ServerSocket(0, 1, InetAddress.getLocalHost());
+        if (ipAddress != null && !ipAddress.isEmpty())
+            this.server = new ServerSocket(65000, 1, InetAddress.getByName(ipAddress));
+        else
+            this.server = new ServerSocket(65000, 1, InetAddress.getLocalHost());
     }
 
     private void listen() throws Exception {
@@ -20,10 +22,10 @@ public class MyServerSocket {
             clientAddress += 1;
         }
     }
-    
+
     private class ClientHandler extends Thread {
         private Socket client;
-        private BufferedReader in;       
+        private BufferedReader in;
         private BufferedWriter out;
         private int clientNumber;
 
@@ -31,37 +33,55 @@ public class MyServerSocket {
             this.client = client;
             this.clientNumber = clientNumber;
             this.in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            this.out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream())); 
-    }
+            this.out = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        }
 
-    @Override
-    public void run() {
-        try {
-            String data;
-            while ((data = in.readLine()) != null) {
-                System.out.println("Message from client " + clientNumber + ": " + data);
-                String response = "Hello " + data;
-                out.write(response);
-                out.newLine();
-                out.flush();
+        @Override
+        public void run() {
+            try {
+                String data;
+                while ((data = in.readLine()) != null) {
+                    System.out.println("Message from client " + clientNumber + ": " + data);
+                    String response = "Hello " + data;
+                    out.write(response);
+                    out.newLine();
+                    out.flush();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
-}
 
-public InetAddress getSocketAddress() {
+    public InetAddress getSocketAddress() {
         return this.server.getInetAddress();
     }
-    
+
     public int getPort() {
         return this.server.getLocalPort();
     }
 
     public static void main(String[] args) throws Exception {
         MyServerSocket app = new MyServerSocket(args[0]);
-        System.out.println("Running Server: " + "Host=" + app.getSocketAddress().getHostAddress() + " Port=" + app.getPort());
+        System.out.println(
+                "Running Server: " + "Host=" + app.getSocketAddress().getHostAddress() + " Port=" + app.getPort());
+        try{
+            File Peers = new File("Peers_list.txt");
+            Scanner myReader = new Scanner(Peers);
+            if(Peers.exists()){
+                System.out.println("Peers List: ");
+                while (myReader.hasNextLine()){
+                    String data = myReader.nextLine();
+                    System.out.println(data);
+                }
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println("File not Found");
+            e.printStackTrace();
+        }
+
         app.listen();
     }
 }
